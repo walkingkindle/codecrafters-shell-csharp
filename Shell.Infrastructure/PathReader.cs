9ebeea1195921689b24f7f7cb9810fcfc66e6f9e
Aleksa <aki.hadza@gmail.com>
2026-02-16 15:18:18 +0100

    home directory

--- Shell.Infrastructure/PathReader.cs
+++ Shell.Infrastructure/PathReader.cs
@@ -10,23 +10,7 @@ namespace Shell.Infrastructure
 
         public string GetHomeDirectory()
         {
-            var homeDrive = Environment.GetEnvironmentVariable("HOMEDRIVE");
-            if (!string.IsNullOrWhiteSpace(homeDrive))
-            {
-                var homePath = Environment.GetEnvironmentVariable("HOMEPATH");
-                if (!string.IsNullOrWhiteSpace(homePath))
-                {
-                    return homeDrive + Path.DirectorySeparatorChar + homePath;
-                }
-                else
-                {
-                    throw new Exception("Environment variable error, there is no 'HOMEPATH'");
-                }
-            }
-            else
-            {
-                throw new Exception("Environment variable error, there is no 'HOMEDRIVE'");
-            }
+            return Environment.GetEnvironmentVariable("HOME");
         }
     }
 }
